8f688ea6ef657e2965d600a297d19f2054dbaf78
anandku06 <161126335+anandku06@users.noreply.github.com>
2025-07-08 22:43:09 +0530

    feat: implement TodoForm and TodoList components with basic functionality

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,15 @@
-import { Button, Stack, Container } from "@chakra-ui/react";
+import { Stack, Container } from "@chakra-ui/react";
 import Navbar  from "./components/Navbar";
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
 
 const App = () => {
   return (
     <Stack h="100vh">
       <Navbar />
       <Container>
-        {/* <TodoForm />
-        <TodoList /> */}
+        <TodoForm />
+        <TodoList />
       </Container>
     </Stack>
   );
