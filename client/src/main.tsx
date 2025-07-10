ba5e81fe5a623bd144de530fdaa4f3a30aa5f66a
anandku06 <161126335+anandku06@users.noreply.github.com>
2025-07-10 12:28:12 +0530

    feat: add React Query for data fetching and state management

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,11 +3,15 @@ import { createRoot } from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
 import "./index.css";
 import App from "./App.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
