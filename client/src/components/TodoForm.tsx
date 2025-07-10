bfa25f504dfd8ee23b904ab8e6967590eecbbf30
anandku06 <161126335+anandku06@users.noreply.github.com>
2025-07-10 18:25:00 +0530

    feat: implement delete functionality for TodoItem with loading state

--- client/src/components/TodoForm.tsx
+++ client/src/components/TodoForm.tsx
@@ -1,20 +1,48 @@
 import React, { useEffect, useRef, useState } from "react";
 import { Flex, Input, Button, Spinner } from "@chakra-ui/react";
 import { IoMdAdd } from "react-icons/io";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { BASE_URL } from "@/App";
 
 const TodoForm = () => {
   const [newTodo, setNewTodo] = useState("");
-  const [isPending, setIsPending] = useState(false);
   const inputRef = useRef<HTMLInputElement>(null);
+  const queryClient = useQueryClient();
 
   useEffect(() => {
     inputRef.current?.focus();
   }, []);
 
-  const createTodo = async (e: React.FormEvent) => {
-    e.preventDefault();
-    alert("Todo added!!");
-  };
+  const { mutate: createTodo, isPending: isCreating } = useMutation({
+    mutationKey: ["createTodo"],
+    mutationFn: async (e: React.FormEvent) => {
+      e.preventDefault();
+
+      try {
+        const res = await fetch(`${BASE_URL}/todos`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ body: newTodo }),
+        });
+
+        const data = await res.json();
+
+        if (!res.ok) throw new Error(data.error || "Something went wrong!");
+
+        setNewTodo("");
+
+        return data;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["todos"] });
+    },
+  });
   return (
     <form onSubmit={createTodo}>
       <Flex gap={2}>
@@ -26,7 +54,7 @@ const TodoForm = () => {
         />
 
         <Button mx={2} type="submit" _active={{ transform: "scale(.97)" }}>
-          {isPending ? <Spinner /> : <IoMdAdd size={30} />}
+          {isCreating ? <Spinner /> : <IoMdAdd size={30} />}
         </Button>
       </Flex>
     </form>
